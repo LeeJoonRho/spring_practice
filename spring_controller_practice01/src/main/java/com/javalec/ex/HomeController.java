2159d520a3fa4331a24d3808366e46dedefa9b64
LeeJoonRho <wnssh607@naver.com>
2017-03-10 15:32:16 +0900

    spring

---
+++ spring_controller_practice01/src/main/java/com/javalec/ex/HomeController.java
@@ -0,0 +1,59 @@
+package com.javalec.ex;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javalec.ex.member.Member;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("/board/checkid")
+	public String checkid(HttpServletRequest httpservletRequest, Model model) {
+		String id = httpservletRequest.getParameter("id");
+		String pw = httpservletRequest.getParameter("pw");
+		
+		model.addAttribute("id", id);
+		model.addAttribute("pw", pw);
+		
+		return "board/checkid";
+	}
+	
+	@RequestMapping("/member/join")
+	public String joinData(Member member) {
+		return "member/join";
+	}
+}
